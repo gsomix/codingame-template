485183cea71912583c0cffe9a142084cb262acf2
Evgeniy Andreev (gsomix) <gsomix@gmail.com>
2020-08-20 16:35:31 +0400

    Measure! Norm! Limit!

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # codingame-template
-F# template for CodinGame bots
+My F# template for [CodinGame](https://www.codingame.com/) bots. Uses simple bot for [Coders Strike Back](https://www.codingame.com/multiplayer/bot-programming/coders-strike-back) game as example.
---
+++ bot.fsx
@@ -0,0 +1,43 @@
+namespace CodersStrikeBack
+
+open System
+
+module Bot =
+    let run() = 
+
+        let mutable prevX = -1
+        let mutable prevY = -1
+        let mutable velX = 0
+        let mutable velY = 0
+
+        while true do
+            let token = (Console.In.ReadLine()).Split [|' '|]
+            let x = int(token.[0])
+            let y = int(token.[1])
+            let nextCheckpointX = int(token.[2])
+            let nextCheckpointY = int(token.[3])
+            let nextCheckpointDist = int(token.[4])
+            let nextCheckpointAngle = int(token.[5])
+            let token1 = (Console.In.ReadLine()).Split [|' '|]
+            let opponentX = int(token1.[0])
+            let opponentY = int(token1.[1])
+
+            if prevX < 0 then prevX <- x
+            if prevY < 0 then prevY <- y    
+
+            velX <- x - prevX
+            velY <- y - prevY
+
+            let targetX = nextCheckpointX - 3*velX
+            let targetY = nextCheckpointY - 3*velY
+
+            printfn "%d %d BOOST" targetX targetY
+            
+            prevX <- x
+            prevY <- y
+
+            ()
+
+
+module EntryPoint = do Bot.run()
+
